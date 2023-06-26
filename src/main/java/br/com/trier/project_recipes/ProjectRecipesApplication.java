479cd8b438793019240939af4fb6e3e5fca2549a
alexgnunes <108927785+alexgnunes@users.noreply.github.com>
2023-06-26 10:54:02 -0300

    first commit

---
+++ src/main/java/br/com/trier/project_recipes/ProjectRecipesApplication.java
@@ -0,0 +1,13 @@
+package br.com.trier.project_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trier/project_recipes/ProjectRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trier.project_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
